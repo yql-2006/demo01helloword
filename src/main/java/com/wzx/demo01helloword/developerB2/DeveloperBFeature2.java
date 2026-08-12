273351b5853efdd757db98d52f12307c7c4c9fd8
DeveloperB <developerB@example.com>
2026-08-12 08:23:21 +0800

    DeveloperB: add second feature

---
+++ src/main/java/com/wzx/demo01helloword/developerB2/DeveloperBFeature2.java
@@ -0,0 +1,7 @@
+﻿package com.wzx.demo01helloword.developerB2;
+
+public class DeveloperBFeature2 {
+    public static void main(String[] args) {
+        System.out.println("Developer B 第二次提交");
+    }
+}
